5cb27f458fccf696738af03271715d89b5235760
G4B1 <gabisantucho000@gmail.com>
2024-10-01 21:56:49 -0300

    realizamos un contador de palabras

---
+++ contador-letras.py
@@ -0,0 +1,11 @@
+# Paso 1: Solicitar entrada al usuario
+
+palabra_ingrasada = input("Por favor, ingrese una palabra: ")
+
+# Paso 2: Contar la cantidad de letras
+
+cantidad_letras = len(palabra_ingrasada)
+
+# Paso 3: Mostrar al usuario el resultado
+
+print("a palabra ingresada tiene", cantidad_letras, "letras.")
